a55f07116215ae0922511c2a33be6b63b66b8a4a
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2026-03-10 07:12:56 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCtFf85MUkNSsSsT6Nv8M_09Fphm2DcQOU",
     authDomain: "dailybriefing-fe7df.firebaseapp.com",
     projectId: "dailybriefing-fe7df",
-    storageBucket: "dailybriefing-fe7df.firebasestorage.app", // Ensure this matches console!
+    storageBucket: "dailybriefing031026.firebasestorage.app", // Ensure this matches console!
     messagingSenderId: "",
     appId: ""
 };
@@ -986,3 +986,4 @@ const applyGlobalStyles = (ws) => {
     const dateStr = new Date().toISOString().split('T')[0];
     XLSX.writeFile(wb, `Briefing_${targetId}_${dateStr}.xlsx`);
 }
+
